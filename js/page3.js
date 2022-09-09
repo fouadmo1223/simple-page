1c50ba4ca80c681f35233e2d700beaa165c59fed
fouadmo1223 <76663024+fouadmo1223@users.noreply.github.com>
2022-09-09 16:01:35 +0200

    Add files via upload

---
+++ js/page3.js
@@ -0,0 +1,15 @@
+let nextButton = document.getElementById("nextButoon");
+let pervButton = document.getElementById("pervButoon");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+
+
+
+nextButton.onclick = function (e) {
+    e.preventDefault()
+}
+
+pervButton.onclick = function (e) {
+    e.preventDefault()
+}
+
